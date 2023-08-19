9060d6a20bf41b046703c476cc6584e192ab1e52
Helder Correia <tuxboy@tlab.pt>
2023-08-19 15:21:18 +0100

    [TASK] Improve deploy script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-rm .editorconfig .env.example .gitattributes .gitignore README.md docker-compose.yml phpstan.neon phpunit.xml renovate.json
-rm -rf tests .github
+# Usage: ./deploy-prod.sh
+# Folder structure
+# main-folder
+#   |-- releases
+#     |-- 20230819_123000
+#   |-- shared
+#     |-- storage
+
+release_name=$(date +"%Y%m%d_%H%M%S")
+mkdir "releases/$release_name"
+
+cd "releases/$release_name" || exit
+
+git clone https://github.com/tuxonice/carob-mailer.git .
+composer install --no-dev
+npm install
+npm run prod
+
+rm .editorconfig .env.example deploy-prod.sh .gitattributes .gitignore README.md docker-compose.yml phpstan.neon phpunit.xml renovate.json
+rm -rf tests .github .git storage
+
+ln -s ../../shared/.env .env
+ln -s ../../shared/storage storage
+
+cd ../../
+
+rm current
+ln -s "releases/$release_name/public/" current
